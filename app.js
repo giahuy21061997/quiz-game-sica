0be77b4356fbfc2b5efba5bd00a03bf1ab9f0eaf
Gia Huy Trần <giahuy21061997@gmail.com>
2024-09-11 23:42:08 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function selectLanguage(language) {
+    if (language === 'vi') {
+        alert('Bạn đã chọn Tiếng Việt');
+        // Add your logic to switch the language or navigate to a different page
+    } else if (language === 'en') {
+        alert('You have selected English');
+        // Add your logic to switch the language or navigate to a different page
+    }
+}
